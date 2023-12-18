551c9cb4c25fb495a595d9f719e446648a347ab2
jonabergamo <jonathanbergamo16@gmail.com>
2023-12-18 18:54:36 -0300

    feat(person): added all CRUD operations

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class PersonService {
 
@@ -16,7 +20,25 @@ public class PersonService {
         this.personDao = personDao;
     }
 
-    public int addPerson(Person person){
+    public int addPerson(Person person) {
         return personDao.insertPerson(person);
     }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDao.updatePersonById(id, newPerson);
+
+    }
+
 }
